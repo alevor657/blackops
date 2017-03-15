52a39e9d52dd239b315ed47ce4267af6bf6b7512
AleVor <alevor657@gmail.com>
2017-03-15 16:24:18 +0100

    Added flashes + dashboard + some minor css changes

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
 
+# Generate a secret word used for sessions
+# app.secret_key = os.urandom(24) Does not work on server
+app.secret_key = "A very secret key"
+
 @app.route('/')
 def index():
-    """ Main page """
+    """ Login page """
+    flash("Bad password, try again")
+    # flash("P.S. admin/admin")
     return render_template('index.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    """ Main dashboard """
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ classes/controller.py
@@ -0,0 +1,5 @@
+class Controller():
+    """
+    Gives control over data to app.py
+    """
+    __init__(self):
